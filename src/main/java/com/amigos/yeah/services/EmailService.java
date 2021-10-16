b3277004d3079c66c62bc044f495f7f0681b6dab
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2021-10-16 20:16:22 -0300

    Sávio-Gomes [Implementação geral  de configurações de Email]

--- src/main/java/com/amigos/yeah/services/EmailService.java
+++ src/main/java/com/amigos/yeah/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.amigos.yeah.services;
 
+import javax.mail.internet.MimeMessage;
+
 import com.amigos.yeah.domain.Pedido;
 
 import org.springframework.mail.SimpleMailMessage;
@@ -10,4 +12,7 @@ public interface EmailService {
 
     void sendEmail(SimpleMailMessage msg);
 
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+    void sendHtmlEmail(MimeMessage msg);
 }
--- src/main/java/com/amigos/yeah/services/PedidoService.java
+++ src/main/java/com/amigos/yeah/services/PedidoService.java
@@ -66,7 +66,7 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
-		emailService.sendOrderConfirmation(obj);
+		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
 }
